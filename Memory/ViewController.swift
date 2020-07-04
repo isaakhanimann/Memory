1f5fd9d6a31554f5f85c173844e264c0b4c242bd
isaaknewton <isaak.hanimann@gmail.com>
2020-07-04 16:45:51 +0200

    Add cards with animation

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -16,10 +16,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let anchorEntity = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(anchorEntity)
+        
+        addCards(to: anchorEntity)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        arView.addGestureRecognizer(tapGestureRecognizer)
+        
+    }
+    
+    func addCards(to anchorEntity: AnchorEntity) {
+        var cards = [Entity]()
+        let numberOfCards = 16
+        
+        let cardTemplate = try! Entity.loadModel(named: "plate")
+        // Generate collision shapes for the card so we can interact with it
+        cardTemplate.generateCollisionShapes(recursive: true)
+        
+        for index in 0..<numberOfCards {
+            let card = cardTemplate.clone(recursive: true)
+            // Give the card a name so we'll know what we're interacting with
+            let firstIndex = index / 2
+            let secondIndex = index % 2
+            card.name = "card\(firstIndex)\(secondIndex)"
+            cards.append(card)
+        }
+        
+        cards.shuffle()
+        
+        for (index, card) in cards.enumerated() {
+            let x = Float(index % 4) - 1.5
+            let z = Float(index / 4) - 1.5
+            // Set the position of the card
+            card.position = [x * 0.1, 0, z * 0.1]
+            // Add the card to the anchor
+            anchorEntity.addChild(card)
+        }
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let tapLocation = recognizer.location(in: arView)
+        // Get the entity at the location we've tapped, if one exists
+        if let card = arView.entity(at: tapLocation) {
+            var cardTransform = card.transform
+            cardTransform.rotation = simd_quatf(angle: .pi, axis: [1,0,0])
+            card.move(to: cardTransform, relativeTo: card.parent, duration: 0.3, timingFunction: .easeInOut)
+        }
+        
+        
+        
+        
     }
 }
