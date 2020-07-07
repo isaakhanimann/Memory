6a6003860372d3aef7bb0a36be33bb952d206a8a
isaaknewton <isaak.hanimann@gmail.com>
2020-07-07 12:42:44 +0200

    Models are on cards

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -14,18 +14,19 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
-    // for loading the models
-    var cancellable: AnyCancellable? = nil
+    var gameAnchor: AnchorEntity!
+    var cards = [Entity]()
+    let numberOfCards = 16
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let anchorEntity = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
+        gameAnchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
         
-        arView.scene.anchors.append(anchorEntity)
+        arView.scene.anchors.append(gameAnchor)
         
-        addEntities(to: anchorEntity)
+        addCardsWithModels()
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
         arView.addGestureRecognizer(tapGestureRecognizer)
@@ -33,54 +34,37 @@ class ViewController: UIViewController {
     }
     
     
-    func addEntities(to anchorEntity: AnchorEntity) {
-        var cards = [Entity]()
-        let numberOfCards = 16
-
+    func addCardsWithModels() {
+        
         let cardTemplate = try! Entity.loadModel(named: "plate")
         // Generate collision shapes for the card so we can interact with it
         cardTemplate.generateCollisionShapes(recursive: true)
 
-        cancellable = Entity.loadModelAsync(named: "toy_robot_vintage")
-            .append(Entity.loadModelAsync(named: "fender_stratocaster"))
-            .append(Entity.loadModelAsync(named: "tv_retro"))
-            .append(Entity.loadModelAsync(named: "cup_saucer_set"))
-            .append(Entity.loadModelAsync(named: "pot_plant"))
-            .append(Entity.loadModelAsync(named: "flower_tulip"))
-            .append(Entity.loadModelAsync(named: "trowel"))
-            .append(Entity.loadModelAsync(named: "teapot"))
-            .collect().sink(receiveCompletion: { completion in
-                if case let .failure(error) = completion {
-                    print("Unable to load a model due to error \(error)")
-                }
-                self.cancellable?.cancel()
-            }, receiveValue: { [self] (models: [Entity]) in
-                self.cancellable?.cancel()
+        let models = [try! Entity.loadModel(named: "toy_robot_vintage"),try! Entity.loadModel(named: "fender_stratocaster"),try! Entity.loadModel(named: "tv_retro"),try! Entity.loadModel(named: "cup_saucer_set"),try! Entity.loadModel(named: "pot_plant"),try! Entity.loadModel(named: "flower_tulip"),try! Entity.loadModel(named: "trowel"),try! Entity.loadModel(named: "teapot")]
+        
+        for index in 0..<self.numberOfCards {
+            let card = cardTemplate.clone(recursive: true)
+            // card00 and card01 correspond to each other
+            let firstIndex = index / 2
+            let secondIndex = index % 2
+            let modelOnCard = models[firstIndex].clone(recursive: true)
+            modelOnCard.position = [0,0.01,0]
+            card.addChild(modelOnCard)
+            // Give the card a name so we'll know what we're interacting with
+            card.name = "card\(firstIndex)\(secondIndex)"
+            self.cards.append(card)
+        }
 
+        self.cards.shuffle()
 
-                for index in 0..<numberOfCards {
-                    let card = cardTemplate.clone(recursive: true)
-                    // card00 and card01 correspond to each other
-                    let firstIndex = index / 2
-                    let secondIndex = index % 2
-                    let modelOnCard = models[firstIndex].clone(recursive: true)
-                    card.addChild(modelOnCard)
-                    // Give the card a name so we'll know what we're interacting with
-                    card.name = "card\(firstIndex)\(secondIndex)"
-                    cards.append(card)
-                }
-
-                cards.shuffle()
-
-                for (index, card) in cards.enumerated() {
-                    let x = Float(index % 4) - 1.5
-                    let z = Float(index / 4) - 1.5
-                    // Set the position of the card
-                    card.position = [x * 0.1, 0, z * 0.1]
-                    // Add the card to the anchor
-                    anchorEntity.addChild(card)
-                }
-            })
+        for (index, card) in self.cards.enumerated() {
+            let x = Float(index % 4) - 1.5
+            let z = Float(index / 4) - 1.5
+            // Set the position of the card
+            card.position = [x * 0.1, 0, z * 0.1]
+            // Add the card to the anchor
+            self.gameAnchor.addChild(card)
+        }
     }
     
     
