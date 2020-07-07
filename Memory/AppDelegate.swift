d041757535b9c58f3603471607763a6460236732
isaaknewton <isaak.hanimann@gmail.com>
2020-07-07 16:07:28 +0200

    Hiding and revealing works

--- Memory/AppDelegate.swift
+++ Memory/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //so code doesn't crash
+        CardComponent.registerComponent()
+        
         return true
     }
 
--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         
         arView.scene.anchors.append(gameAnchor)
         
+        
         addCardsWithModels()
         addOcclusionBox()
         
@@ -38,25 +39,26 @@ class ViewController: UIViewController {
     
     func addCardsWithModels() {
         
-        let cardTemplate = try! Entity.loadModel(named: "plate")
+        let cardModelEntity = try! Entity.loadModel(named: "plate")
+
+        let cardEntityTemplate = CardEntity()
+        cardEntityTemplate.model = cardModelEntity.model
+        cardEntityTemplate.collision = cardModelEntity.collision
+        cardEntityTemplate.card = CardComponent()
         // Generate collision shapes for the card so we can interact with it
-        cardTemplate.generateCollisionShapes(recursive: true)
-        cardTemplate.transform.rotation = simd_quatf(angle: .pi, axis: [1,0,0])
-
-
+        cardEntityTemplate.generateCollisionShapes(recursive: true)
+        cardEntityTemplate.transform.rotation = simd_quatf(angle: .pi, axis: [1,0,0])
+            
         let models = [try! Entity.loadModel(named: "toy_robot_vintage"),try! Entity.loadModel(named: "fender_stratocaster"),try! Entity.loadModel(named: "tv_retro"),try! Entity.loadModel(named: "cup_saucer_set"),try! Entity.loadModel(named: "pot_plant"),try! Entity.loadModel(named: "flower_tulip"),try! Entity.loadModel(named: "trowel"),try! Entity.loadModel(named: "teapot")]
         
         for index in 0..<self.numberOfCards {
-            let card = cardTemplate.clone(recursive: true)
-            // card00 and card01 correspond to each other
-            let firstIndex = index / 2
-            let secondIndex = index % 2
-            let modelOnCard = models[firstIndex].clone(recursive: true)
+            let card = cardEntityTemplate.clone(recursive: true)
+            let modelIndex = index / 2
+            let modelOnCard = models[modelIndex].clone(recursive: true)
             // +0.001 so the model hovers just a little bit over the card so the occlusionBox occludes it
             modelOnCard.position = [0,cardThickness/2+0.001,0]
             card.addChild(modelOnCard)
-            // Give the card a name so we'll know what we're interacting with
-            card.name = "card\(firstIndex)\(secondIndex)"
+            card.components[CardComponent.self]?.kind = modelOnCard.name
             self.cards.append(card)
         }
 
@@ -77,18 +79,13 @@ class ViewController: UIViewController {
     @objc func handleTap(recognizer: UITapGestureRecognizer) {
         let tapLocation = recognizer.location(in: arView)
         // Get the entity at the location we've tapped, if one exists
-        if let card = arView.entity(at: tapLocation) {
-            var cardTransform = card.transform
-            if true {
-                // Set the card to rotate back to 0 degrees
-                cardTransform.rotation = simd_quatf(angle: 0, axis: [1, 0, 0])
+        if let cardEntity = arView.entity(at: tapLocation) as? CardEntity {
+            if cardEntity.card.revealed {
+                cardEntity.hide()
             } else {
-                cardTransform.rotation = simd_quatf(angle: .pi, axis: [1,0,0])
-
+                cardEntity.reveal()
             }
-            card.move(to: cardTransform, relativeTo: card.parent, duration: 0.3, timingFunction: .easeInOut)
         }
-        
     }
     
     func addOcclusionBox() {
@@ -100,4 +97,39 @@ class ViewController: UIViewController {
         gameAnchor.addChild(occlusionBox)
     }
 
+}
+
+
+// Declare custom entity with the Model, Collision and Card Component
+class CardEntity: Entity, HasModel, HasCollision {
+    
+    // for convenient access to card state
+    public var card: CardComponent {
+        get { return components[CardComponent.self] ?? CardComponent() }
+        set { components[CardComponent.self] = newValue }
+    }
+    
+    func reveal() {
+        card.revealed = true
+        
+        // transform is a value type so this copies it
+        var cardTransform = self.transform
+        cardTransform.rotation = simd_quatf(angle: 0, axis: [1, 0, 0])
+        move(to: cardTransform, relativeTo: parent, duration: 0.3, timingFunction: .easeInOut)
+    }
+    
+    func hide() {
+        card.revealed = false
+        
+        // transform is a value type so this copies it
+        var cardTransform = self.transform
+        cardTransform.rotation = simd_quatf(angle: .pi, axis: [1, 0, 0])
+        move(to: cardTransform, relativeTo: parent, duration: 0.3, timingFunction: .easeInOut)
+    }
+}
+
+
+struct CardComponent: Component, Codable {
+    var revealed = false
+    var kind = ""
 }
